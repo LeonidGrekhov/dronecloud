3ea57d53a24973a7079435248ff3832d746cfe3b
prernagarsole <99928364+prernagarsole@users.noreply.github.com>
2022-11-13 20:01:54 -0800

    Step3 drone catlog

---
+++ src/pages/contents.js
@@ -0,0 +1,32 @@
+import React from "react"
+import "./contents.css"
+
+export default function Contents() {
+  return (
+    <div className="contents flex-col-hstart-vstart">
+      <div className="progress-bar flex-row-vcenter-hstart">
+        <div className="active-indicator" />
+        <div className="active-line" />
+        <img
+          src="https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/fly8f9uvtvn-1838%3A3568?alt=media&token=b5472822-62ce-4b83-a32d-8e2868da3f95"
+          alt="Not Found"
+          className="active-indicator-1"
+        />
+        <div className="default-indicator" />
+        <div className="active-line" />
+        <div className="default-indicator-1" />
+        <div className="active-line" />
+        <div className="default-indicator-2" />
+      </div>
+      <p className="txt-817">Step 3: Selected Drone</p>
+            
+              <p className="txt-706">
+                This is your selected drone.
+              </p>
+
+              
+        </div>
+        
+    
+  )
+}
